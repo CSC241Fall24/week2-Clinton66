f9324e6f3bb9168e12469819687491903fee048e
Clinton66 <113050184+Clinton66@users.noreply.github.com>
2024-10-04 19:50:35 -0500

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -3,28 +3,26 @@
 public class Q1WeirdFunction {
 
     public static int fRecursive(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            return fRecursive(n - 1) + fRecursive(n - 2) + n;
-        }
-    }
+      if (n < 3) {
+return n ;
+}
+return fRecursive( n - 1) + 2 * fRecursive(n - 2) + 3 * fRecursive(n - 3);
+}
+
+    
 
     public static int fIterative(int n) {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-
-        int a = 0, b = 1, result = 0;
-        for (int i = 2; i <= n; i++) {
-            result = b + a + i;
-            a = b;
-            b = result;
-        }
-        return result;
+        if ( n < 3) {
+return n ;  }
+int [] values = new int [ n + 1];
+values [0] = 0;
+values [1] = 1;
+values [2] = 2;
+    for ( int i = 3; i <= n ; i ++) {
+values [ i ] = values [ i - 1] + 2 * values [ i - 2] + 3 * values [ i - 3];
+}
+    return values [ n ];
     }
-
     public static void main(String[] args) {
         int n = 4;
         System.out.println("Recursive: " + fRecursive(n));
